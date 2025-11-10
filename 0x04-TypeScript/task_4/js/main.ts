c4175b06025618f20628f7012d97663391a67e98
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2025-11-10 12:34:19 +0100

    done

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,11 +1,16 @@
-const Cpp = Subjects.Cpp;;
+/// <reference path="./subjects/Subject.ts" />
+/// <reference path="./subjects/Teacher.ts" />
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
+
+const Cpp = Subjects.Cpp;
 const Java = Subjects.Java;
 const React = Subjects.React;
 
 export const cpp = new Cpp();
 export const java = new Java();
 export const react = new React();
-
 
 export const cTeacher: Subjects.Teacher = {
   firstName: "John",
